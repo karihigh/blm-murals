3773d3aed62d4213a0de404f308d875b5d8f0990
Karina Hyland <khyland111@gmail.com>
2020-08-05 14:48:19 -0400

    images added to map

--- script.js
+++ script.js
@@ -1,399 +1,264 @@
 let mydata = [
   {
-    "#": 1,
+    "ID": 1,
     "Name": "Black Lives Plaza",
     "State": "Washington DC",
-    "Country": "United States",
     "Latitude": 38.90132849,
     "Longitude": -77.03652978,
-    "Notes": "Yellow",
-    "Source1": "https://www.instagram.com/p/CCs82I4huNH/",
-    "Source2": "https://www.instagram.com/p/CCrxJ-5lpTP/",
-    "Source3": "https://thespaces.com/a-50-ft-tall-black-lives-matter-mural-appears-next-to-the-white-house/"
+    "File": "01.jpg",
+    "Source": "@CheriJacobus"
   },
   {
-    "#": 2,
+    "ID": 2,
     "Name": "5th Ave",
     "State": "New York",
-    "Country": "United States",
     "Latitude": 40.76256859,
     "Longitude": -73.97425175,
-    "Notes": "Yellow",
-    "Source1": "https://www.instagram.com/p/CCs_TNQHsSj/",
-    "Source2": "",
-    "Source3": ""
+    "File": "02.jpg",
+    "Source": "@nymmg"
   },
   {
-    "#": 3,
+    "ID": 3,
     "Name": "Joralemon St.",
     "State": "New York",
-    "Country": "United States",
     "Latitude": 40.69245287,
     "Longitude": -73.99033159,
-    "Notes": "Yellow",
-    "Source1": "https://www.instagram.com/p/CB6BD0YBBva/",
-    "Source2": "",
-    "Source3": ""
+    "File": "03.jpg",
+    "Source": "Filip Wolak"
   },
   {
-    "#": 4,
+    "ID": 4,
     "Name": "Harlem",
     "State": "New York",
-    "Country": "United States",
     "Latitude": 40.80682568,
     "Longitude": -73.9461422,
-    "Notes": "Various colors",
-    "Source1": "https://www.instagram.com/p/CCOqr74JiKP/",
-    "Source2": "https://www.wionews.com/videos/black-lives-matter-mural-unveiled-in-harlem-312052",
-    "Source3": "https://gothamist.com/arts-entertainment/videos-drone-footage-captures-black-lives-matter-street-murals-across-nyc"
+    "File": "04.jpg",
+    "Source": "ALON SICHERMAN"
   },
   {
-    "#": 5,
+    "ID": 5,
     "Name": "Foley Square",
     "State": "New York",
-    "Country": "United States",
     "Latitude": 40.71477717,
     "Longitude": -74.00277972,
-    "Notes": "Various colors",
-    "Source1": "https://www.instagram.com/p/CCOKnMcl4VH/",
-    "Source2": "",
-    "Source3": ""
+    "File": "05.jpg",
+    "Source": "Topiary Productions / Manhattan Borough President"
   },
   {
-    "#": 6,
+    "ID": 6,
     "Name": "Bed-Stuy",
     "State": "New York",
-    "Country": "United States",
     "Latitude": 40.68014171,
     "Longitude": -73.94486547,
-    "Notes": "Yellow",
-    "Source1": "https://www.instagram.com/p/CCM1eGWDl5q/",
-    "Source2": "https://www.amny.com/news/black-lives-matter-mural-painted-on-block-long-stretch-of-brooklyns-fulton-street/",
-    "Source3": ""
+    "File": "06.jpg",
+    "Source": "Paul Frangipane"
   },
   {
-    "#": 7,
+    "ID": 7,
     "Name": "Worcester",
     "State": "Massachusetts",
-    "Country": "United States",
     "Latitude": 42.26206551,
     "Longitude": -71.79977417,
-    "Notes": "Various colors",
-    "Source1": "https://www.instagram.com/p/CCs8qbYnjbB/",
-    "Source2": "",
-    "Source3": ""
+    "File": "07.jpg",
+    "Source": "@jpjacobs22"
   },
   {
-    "#": 8,
+    "ID": 8,
     "Name": "Montreal",
     "State": "Canada",
-    "Country": "Canada",
     "Latitude": 45.49527771,
     "Longitude": -73.60977173,
-    "Notes": "White",
-    "Source1": "https://www.instagram.com/p/CCs5WS0j7Zi/",
-    "Source2": "",
-    "Source3": ""
+    "File": "08.jpg",
+    "Source": "@Lea_Kim"
   },
   {
-    "#": 9,
+    "ID": 9,
     "Name": "Los Angeles",
     "State": "California",
-    "Country": "United States",
     "Latitude": 34.0522342,
     "Longitude": -118.2436849,
-    "Notes": "Yellow",
-    "Source1": "https://www.instagram.com/p/CCsd1oRn6jt/",
-    "Source2": "",
-    "Source3": ""
+    "File": "09.jpg",
+    "Source": "ROBYN BECK/GETTY"
   },
   {
-    "#": 10,
+    "ID": 10,
     "Name": "Tallahassee",
     "State": "Florida",
-    "Country": "United States",
     "Latitude": 30.4382559,
     "Longitude": -84.2807329,
-    "Notes": "Yellow",
-    "Source1": "https://www.instagram.com/p/CCZmDNEH66t/",
-    "Source2": "",
-    "Source3": ""
+    "File": "10.jpg",
+    "Source": "@megaAceMedia"
   },
   {
-    "#": 11,
+    "ID": 11,
     "Name": "Palo Alto",
     "State": "California",
-    "Country": "United States",
     "Latitude": 37.4418834,
     "Longitude": -122.1430195,
-    "Notes": "Various colors",
-    "Source1": "https://www.instagram.com/p/CCKE12wBv8v/",
-    "Source2": "",
-    "Source3": ""
+    "File": "11.jpg",
+    "Source": "Benny Villareal"
   },
   {
-    "#": 12,
+    "ID": 12,
     "Name": "Chicago",
     "State": "Illinois",
-    "Country": "United States",
     "Latitude": 41.8781136,
     "Longitude": -87.6297982,
-    "Notes": "Blue",
-    "Source1": "https://twitter.com/ObamaFoundation/status/1286358933024772097/photo/1",
-    "Source2": "",
-    "Source3": ""
+    "File": "12.jpg",
+    "Source": "@ObamaFoundation"
   },
   {
-    "#": 13,
-    "Name": "Durham",
-    "State": "North Carolina",
-    "Country": "United States",
-    "Latitude": 35.9940329,
-    "Longitude": -78.898619,
-    "Notes": "Yellow, DEFUND",
-    "Source1": "https://twitter.com/JeanBeanMosher/status/1285271221110800384",
-    "Source2": "https://twitter.com/KristenAbigail/status/1274336130704592897/photo/1",
-    "Source3": ""
-  },
-  {
-    "#": 14,
+    "ID": 14,
     "Name": "Sandra Bland Parkway",
     "State": "Texas",
-    "Country": "United States",
     "Latitude": 31.9685988,
     "Longitude": -99.9018131,
-    "Notes": "Yellow",
-    "Source1": "https://twitter.com/tpp1876/status/1281271039553568768/photo/1",
-    "Source2": "",
-    "Source3": ""
+    "File": "14.jpg",
+    "Source": "@tpp1876"
   },
   {
-    "#": 15,
+    "ID": 15,
     "Name": "East Pine Street, Seattle",
     "State": "Washington",
-    "Country": "United States",
     "Latitude": 47.6062095,
     "Longitude": -122.3320708,
-    "Notes": "Various colors",
-    "Source1": "https://www.cnn.com/2020/06/14/us/seattle-black-lives-matter-mural-trnd/",
-    "Source2": "",
-    "Source3": ""
+    "File": "15.jpg",
+    "Source": "@kylekotajarvi"
   },
   {
-    "#": 16,
+    "ID": 16,
     "Name": "San Francisco",
     "State": "California",
-    "Country": "United States",
     "Latitude": 37.7749295,
     "Longitude": -122.4194155,
-    "Notes": "Yellow",
-    "Source1": "https://www.forbes.com/sites/jemimamcevoy/2020/06/16/black-lives-matter-murals-color-streets-across-the-country-photos/#800c57e7c03f",
-    "Source2": "https://twitter.com/Ah_lee_Zair/status/1271884493092081666",
-    "Source3": ""
+    "File": "16.jpg",
+    "Source": "Benny Villareal"
   },
   {
-    "#": 18,
+    "ID": 18,
     "Name": "Oakland",
     "State": "California",
-    "Country": "United States",
     "Latitude": 37.8043514,
     "Longitude": -122.2711639,
-    "Notes": "Yellow",
-    "Source1": "https://www.forbes.com/sites/jemimamcevoy/2020/06/16/black-lives-matter-murals-color-streets-across-the-country-photos/#800c57e7c03f",
-    "Source2": "https://www.demilked.com/black-lives-matter-murals-usa/",
-    "Source3": ""
+    "File": "18.jpg",
+    "Source": "HEARST NEWSPAPERS"
   },
   {
-    "#": 19,
+    "ID": 19,
     "Name": "St. Petersburgh",
     "State": "Florida",
-    "Country": "United States",
     "Latitude": 27.76619064,
-    "Longitude": -82.6679992,
-    "Notes": "Various colors",
-    "Source1": "https://www.cltampa.com/news-views/local-news/article/21137765/st-pete-now-has-a-giant-black-lives-matter-street-mural",
-    "Source2": "https://www.wfla.com/news/pinellas-county/city-employees-paint-black-lives-matter-mural-in-st-pete/?fbclid=IwAR2wVaNmVpGg-gY2KB9ldhD7MnAzR9SiDQmCLs66_zJIID4FE06wIR9UIDs",
-    "Source3": ""
+    "Longitude": -82.66799927,
+    "File": "19.jpg",
+    "Source": "BRYAN EDWARD CREATIVE"
   },
   {
-    "#": 20,
+    "ID": 20,
     "Name": "Charlotte",
     "State": "North Carolina",
-    "Country": "United States",
     "Latitude": 35.2270869,
     "Longitude": -80.8431267,
-    "Notes": "Various colors",
-    "Source1": "https://www.instagram.com/p/CBQeT8nhTDV/",
-    "Source2": "https://qcitymetro.com/2020/06/09/black-lives-matter-mural-covers-uptown-charlotte-street/",
-    "Source3": "https://twitter.com/travisakers/status/1270695558714470400"
+    "File": "19.jpg",
+    "Source": "Maleek Loyd"
   },
   {
-    "#": 21,
+    "ID": 21,
     "Name": "Dallas",
     "State": "Texas",
-    "Country": "United States",
     "Latitude": 32.7766642,
     "Longitude": -96.7969879,
-    "Notes": "Black, green, red",
-    "Source1": "https://www.demilked.com/black-lives-matter-murals-usa/",
-    "Source2": "",
-    "Source3": ""
+    "File": "20.jpg",
+    "Source": "@jeffamador"
   },
   {
-    "#": 22,
+    "ID": 22,
     "Name": "Rochester",
     "State": "New York",
-    "Country": "United States",
     "Latitude": 43.1565779,
     "Longitude": -77.6088465,
-    "Notes": "Blue",
-    "Source1": "https://www.demilked.com/black-lives-matter-murals-usa/",
-    "Source2": "https://twitter.com/shawndunwoody/status/1271445770424725504",
-    "Source3": ""
+    "File": "22.jpg",
+    "Source": "@shawndunwoody"
   },
   {
-    "#": 23,
+    "ID": 23,
     "Name": "Sacramento",
     "State": "California",
-    "Country": "United States",
     "Latitude": 38.5815719,
     "Longitude": -121.4943996,
-    "Notes": "Black over yellow",
-    "Source1": "https://www.demilked.com/black-lives-matter-murals-usa/",
-    "Source2": "",
-    "Source3": ""
+    "File": "23.jpg",
+    "Source": "@GuysontheGrid"
   },
   {
-    "#": 24,
+    "ID": 24,
     "Name": "Montpelier",
     "State": "Vermont",
-    "Country": "United States",
     "Latitude": 38.2188244,
     "Longitude": -78.1684568,
-    "Notes": "Yellow",
-    "Source1": "https://www.demilked.com/black-lives-matter-murals-usa/",
-    "Source2": "",
-    "Source3": ""
+    "File": "24.jpg",
+    "Source": "@AlexWhitcomb"
   },
   {
-    "#": 25,
+    "ID": 25,
     "Name": "Denver",
     "State": "Colorado",
-    "Country": "United States",
     "Latitude": 39.7392358,
     "Longitude": -104.990251,
-    "Notes": "Black over white",
-    "Source1": "https://www.demilked.com/black-lives-matter-murals-usa/",
-    "Source2": "https://denver.cbslocal.com/2020/06/12/denver-black-lives-matter-street-mural-broadway/",
-    "Source3": "https://twitter.com/alex_burness/status/1271471932458835969"
+    "File": "25.jpg",
+    "Source": "@PortiaColorado"
   },
   {
-    "#": 26,
+    "ID": 26,
     "Name": "Raleigh",
     "State": "North Carolina",
-    "Country": "United States",
     "Latitude": 35.7795897,
     "Longitude": -78.6381787,
-    "Notes": "Yellow \"End Racism Now",
-    "Source1": "https://www.demilked.com/black-lives-matter-murals-usa/",
-    "Source2": "https://twitter.com/ayegina_/status/1269971406315536385",
-    "Source3": ""
+    "File": "26.jpg",
+    "Source": "@NewsO_DaveH"
   },
   {
-    "#": 27,
+    "ID": 27,
     "Name": "Albany",
     "State": "New York",
-    "Country": "",
     "Latitude": 42.6525793,
     "Longitude": -73.7562317,
-    "Notes": "Yellow",
-    "Source1": "https://www.demilked.com/black-lives-matter-murals-usa/",
-    "Source2": "https://twitter.com/CBS6Leanne/status/1270414199286632448?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1270414199286632448%7Ctwgr%5E&ref_url=https%3A%2F%2Fpeople.com%2Fcrime%2Fphotos-of-black-lives-matter-murals-across-the-country%2F",
-    "Source3": ""
+    "File": "27.jpg",
+    "Source": "@gunclemicah"
   },
   {
-    "#": 28,
-    "Name": "Winston-Salem",
-    "State": "North Carolina",
-    "Country": "",
-    "Latitude": 36.0998596,
-    "Longitude": -80.244216,
-    "Notes": "Various colors \"End Racism Now",
-    "Source1": "https://twitter.com/ayegina_/status/1269971406315536385",
-    "Source2": "",
-    "Source3": ""
-  },
-  {
-    "#": 29,
-    "Name": "Topeka",
-    "State": "Kansas",
-    "Country": "",
-    "Latitude": 39.0473451,
-    "Longitude": -95.6751576,
-    "Notes": "Yellow",
-    "Source1": "https://twitter.com/hannahKSNT/status/1270040683450109953",
-    "Source2": "",
-    "Source3": ""
-  },
-  {
-    "#": 30,
+    "ID": 30,
     "Name": "Berkeley",
     "State": "California",
-    "Country": "",
     "Latitude": 37.8715226,
     "Longitude": -122.273042,
-    "Notes": "Yellow",
-    "Source1": "https://twitter.com/abc7newsbayarea/status/1270554287832043521",
-    "Source2": "",
-    "Source3": ""
+    "File": "30.jpg",
+    "Source": "Benny Villareal"
   },
   {
-    "#": 31,
-    "Name": "Hollywood",
-    "State": "California",
-    "Country": "",
-    "Latitude": 34.0928092,
-    "Longitude": -118.3286614,
-    "Notes": "Various colors",
-    "Source1": "https://people.com/crime/photos-of-black-lives-matter-murals-across-the-country/",
-    "Source2": "",
-    "Source3": ""
-  },
-  {
-    "#": 32,
+    "ID": 32,
     "Name": "Orlando",
     "State": "Florida",
-    "Country": "",
     "Latitude": 28.5383355,
     "Longitude": -81.3792365,
-    "Notes": "Black, green, red",
-    "Source1": "https://www.wogx.com/news/downtown-orlando-black-lives-matter-street-mural-defaced-police-say",
-    "Source2": "",
-    "Source3": ""
+    "File": "32.jpg",
+    "Source": "FOX 35"
   },
   {
-    "#": 33,
+    "ID": 33,
     "Name": "Martinez",
     "State": "California",
-    "Country": "",
     "Latitude": 38.0193657,
     "Longitude": -122.1341321,
-    "Notes": "Yellow",
-    "Source1": "https://www.today.com/news/two-accused-painting-over-black-lives-matter-mural-charged-hate-t186145",
-    "Source2": "",
-    "Source3": ""
+    "File": "33.jpg",
+    "Source": "@mtz.for.black.lives"
   },
   {
-    "#": 34,
+    "ID": 34,
     "Name": "Kalamazoo",
     "State": "Michigan",
-    "Country": "",
     "Latitude": 42.2917069,
     "Longitude": -85.5872286,
-    "Notes": "Various colors",
-    "Source1": "https://www.woodtv.com/news/kzoo-and-bc/volunteers-paint-black-lives-matter-mural-in-kalamazoo/",
-    "Source2": "",
-    "Source3": ""
+    "File": "34.jpg",
+    "Source": "Kyle Mitchell"
   }
 ];
 
@@ -401,25 +266,35 @@ let mydata = [
 let mytoken = "pk.eyJ1Ijoia2FyaWhpZ2giLCJhIjoiY2p1bGpnbTByMGZ1czRkbHI3aDBwdHk1eiJ9.KJSyhm6rfumygW_BhUDHMQ";
 
 // Initialize Leaflet, declare the ID that from the HTML div, set initial view to whatever location, tihs is ITP, and zoom
-let mymap = L.map('mapid').setView([40.6933, -73.9874], 2);
+let mymap = L.map('mapid').setView([39.04, -95.67], 4);
+
+let fistIcon = L.icon({
+  iconUrl: 'icon.png',
+  iconSize: [22, 31],
+  iconAnchor: [10, 10],
+  popupAnchor: [0, 0],
+});
+
 
 // Initialize the base tiles
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/outdoors-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: mytoken
 }).addTo(mymap);
+
+
 
 // Loop through the dataset and create markers and popups
 for (let i = 0; i < mydata.length; i++) {
 
   let myplace = mydata[i];
 
-  let popupText = "<h3>" + myplace.Name + "</h3>" + myplace.State;
+  let popupText = myplace.Name + ", " + myplace.State + "</br>" + "Image Credit: " + myplace.Source + "<img src='" + "assets/" + myplace.File + "'>";
 
-  L.marker([myplace.Latitude, myplace.Longitude]).bindPopup(popupText).addTo(mymap);
+  L.marker([myplace.Latitude, myplace.Longitude], {icon: fistIcon}).bindPopup(popupText).addTo(mymap);
 
 }
